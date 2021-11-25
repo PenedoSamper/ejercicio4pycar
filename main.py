250a3927be24191649a4bbb58b5bcc10571908b9
PenedoSamper <91224646+PenedoSamper@users.noreply.github.com>
2021-11-25 11:11:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from main_functions import *
 pygame.init()
 
 #call main routine
-size = [1300, 600] # Define size of windows
-ancho =  int(input("ancho de la ventana: "))
-alto = int(input("alto de la venta: "))
-size[0] = ancho
-size[1] = alto
-main2(size)
+ancho= int(input("ancho de la ventana:" )
+alto= int(input("alto de la ventana: ")
+size= (ancho, alto)
+azul= (0, 0, 255)
+titulo= input("título del simulador:")
+main2(size, titulo, azul)
